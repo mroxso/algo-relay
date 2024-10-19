9ba7941a4eeecde09a4fd6277d42b6335fc7a8d3
Barry Deen <barrydeen@protonmail.com>
2024-10-19 14:00:10 -0400

    remove print

--- algorithm.go
+++ algorithm.go
@@ -51,7 +51,6 @@ func GetUserFeed(ctx context.Context, userID string, limit int) ([]nostr.Event, 
 
 	var result []nostr.Event
 	for i, feedPost := range filteredFeed {
-		log.Println("post", feedPost.Event.ID, "score", feedPost.Score)
 		if i >= limit {
 			break
 		}
